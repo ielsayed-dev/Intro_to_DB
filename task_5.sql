ea21583cf23973d51eeba96dfdcbb19d19bafba2
Islam Elsayed <110628450+ielsayed-dev@users.noreply.github.com>
2025-10-20 00:27:15 +0300

    customer not customers?

--- task_5.sql
+++ task_5.sql
@@ -1,6 +1,6 @@
 -- SET THE TARGET DATABASE
 USE alx_book_store;
 
--- Inserts a single row into the CUSTOMERS table.
-INSERT INTO Customers (customer_id, customer_name, email, address) 
+-- Insert a single row into the customer table
+INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES (1, 'Cole Baidoo', 'cbaidoo@sandtech.com', '123 Happiness Ave.');
