f6c3b81f7ad97158fda619ce967cb575270c48e2
Islam Elsayed <110628450+ielsayed-dev@users.noreply.github.com>
2025-10-20 00:36:57 +0300

    testing 3

--- task_6.sql
+++ task_6.sql
@@ -1,5 +1,5 @@
 -- SET THE TARGET DATABASE
-USE alx_book_store;
+-- USE alx_book_store;
 
 /*
 -- Insert multiple rows into the customer table
@@ -10,8 +10,18 @@ VALUES
     (4, `Nehemial Kamolu`, `nkamolu@sandtech.com`, `126 Happiness Ave.`);
 */
 
+/*
 INSERT INTO customer (customer_id, customer_name, email, address)
 VALUES 
     ('2', 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness Ave.'),
     ('3', 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness Ave.'),
     ('4', 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness Ave.');
+*/
+
+USE alx_book_store;
+
+INSERT INTO customers (customer_id, customer_name, email, address)
+VALUES 
+(2, 'Blessing Malik', 'bmalik@sandtech.com', '124 Happiness  Ave.'),
+(3, 'Obed Ehoneah', 'eobed@sandtech.com', '125 Happiness  Ave.'),
+(4, 'Nehemial Kamolu', 'nkamolu@sandtech.com', '126 Happiness  Ave.');
